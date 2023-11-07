80861b768319d691b89a4247ad28172f14e4843d
GabrielFariaszz <98537407+GabrielFariaszz@users.noreply.github.com>
2023-11-07 15:00:09 -0300

    Update BancoDeDados.py

--- BancoDeDados.py
+++ BancoDeDados.py
@@ -1,15 +1,22 @@
 class BancoDeDados:
     def __init__(self, lista_usuarios = [], lista_materias = [], lista_professores = []):
-        self.__lista_usuarios = lista_usuarios
+        self.lista_usuarios = lista_usuarios
         self.lista_materias = lista_materias
         self.lista_professores = lista_professores
 
+    def adc_usuarios(self, usuario):
+        self.lista_usuarios.append(usuario)
+
     def listar_usuarios(self):
-        pass
+        print('         Lista de usuários         ')
+        print('--------------------------------')
+        for i in self.lista_usuarios:
+            print(f"Nome de usuário: {i.nome_de_usuario}")
+        print('--------------------------------')
+
     
     def listar_materias(self):
         pass
 
     def listar_professores(self):
         pass
-    
