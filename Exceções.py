cf78432f38366b14cc390a8e46933048f659ae01
GabrielFariaszz <98537407+GabrielFariaszz@users.noreply.github.com>
2023-11-12 14:17:39 -0300

    Update Exceções.py

--- Exceções.py
+++ Exceções.py
@@ -1,2 +1,11 @@
 class TimMaiaError(Exception):
     pass
+
+class LoginEmptyError(Exception):
+    pass
+
+class LoginUserError(Exception):
+    pass
+
+class LoginPasswordError(Exception):
+    pass
