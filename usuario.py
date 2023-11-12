223063479c9a173b4aa9e33ee8d1b413fb5fa755
GabrielFariaszz <98537407+GabrielFariaszz@users.noreply.github.com>
2023-11-12 14:18:37 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,10 +1,22 @@
 from abc import ABC, abstractmethod
 
 class Usuario:
-    def __init__(self, nome, senha, nome_de_usuario):
+    def __init__(self, nome_de_usuario, senha, nome = ''):
         self._nome = nome
         self.__senha = senha
         self.nome_de_usuario = nome_de_usuario
+        self.logado = False
+
+    @property
+    def senha(self):
+        return self.__senha
+    
+    @senha.setter
+    def senha(self,nova_senha):
+        self.__senha = nova_senha
+
+    def logar(self):
+        self.logado = True
 
     def feedback_professor(self):
         pass
@@ -13,8 +25,8 @@ class Usuario:
         pass
 
 class Administrador(Usuario):
-    def __init__(self, nome, senha, nome_de_usuario):
-        super().__init__(nome, senha, nome_de_usuario)
+    def __init__(self, nome_de_usuario, senha, nome = ''):
+        super().__init__(nome_de_usuario, senha, nome = '')
 
     def adicionar_professor(self):
         pass
@@ -29,8 +41,8 @@ class Administrador(Usuario):
         pass
 
 class User(Usuario):
-    def __init__(self, nome, senha, nome_de_usuario):
-        super().__init__(nome, senha, nome_de_usuario)
+    def __init__(self, nome_de_usuario, senha, nome = ''):
+        super().__init__(nome_de_usuario, senha, nome = '')
 
     def trocar_nome(self):
         pass
